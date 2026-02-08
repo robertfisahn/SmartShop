5889e218731a47dec1baa9289ce3765ac98eb361
Robert Fisahn <robertfisahn@gmail.com>
2026-02-08 11:34:30 +0100

    refactor: extract Fluent API configurations from DbContext

---
+++ SmartShopAPI/SmartShopAPI/Data/Configurations/RoleConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using SmartShopAPI.Entities;
+
+namespace SmartShopAPI.Data.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
---
+++ SmartShopAPI/SmartShopAPI/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using SmartShopAPI.Entities;
+
+namespace SmartShopAPI.Data.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<User> builder)
+        {
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.Property(u => u.PasswordHash)
+                .IsRequired()
+                .HasMaxLength(200);
+            builder.Property(u => u.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.Property(u => u.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+            builder.HasOne(u => u.Role)
+                .WithMany()
+                .HasForeignKey(u => u.RoleId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
